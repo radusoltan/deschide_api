d91714e364ae04272a592b1a487736a3f72b09c6
radusoltan <radusoltan@gmail.com>
2022-02-18 23:07:08 +0100

    first commit

---
+++ app/Http/Controllers/ApiAuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiAuthController extends Controller
+{
+    public function login(Request $request){
+
+        dump($request);
+
+    }
+}
