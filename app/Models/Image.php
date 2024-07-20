64d4f22a4d678b9979224fd65b6dab4f15b503bd
Radu Soltan <radusoltan@gmail.com>
2024-07-20 02:01:09 +0200

    deploy 1.5; - updated Article Model;

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,7 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'path', 'width', 'height', 'old_number', 'author', 'source', 'description'];
+    protected $fillable = [
+        'name',
+        'path',
+        'width',
+        'height',
+        'old_number',
+        'author',
+        'source',
+        'description'
+    ];
 
     public function articles() {
         return $this->belongsToMany(Article::class, 'article_images', 'image_id',"article_id")->withPivot('is_main');
